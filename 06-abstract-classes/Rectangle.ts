f3ad698226d6e05c752d92110e09526b0cdb806e
haein_sung <blue6zombie6@gmail.com>
2024-10-15 16:02:56 +0900

    abstract class start

---
+++ 06-abstract-classes/Rectangle.ts
@@ -0,0 +1,28 @@
+import { Shape } from "./Shape";
+
+export class Rectangle extends Shape {
+
+    constructor(theX: number, theY: number, private _width: number, private _height: number) {
+        super(theX, theY);
+    }
+
+    get width(): number {
+        return this._width;
+    }
+
+    set width(value: number) {
+        this._width = value;
+    }
+
+    get height(): number {
+        return this._height;
+    }
+
+    set height(value: number) {
+        this._height = value;
+    }
+
+    getInfo(): string {
+        return super.getInfo() + `, width=${this._width}, height=${this._height}`;
+    }
+}
