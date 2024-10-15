331591dbbe31fb276cbbca185afbd7669da60824
haein_sung <blue6zombie6@gmail.com>
2024-10-15 14:30:24 +0900

    first commit typescript basics just before creating classes

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.66;
+var firstName = "Anup";
+var lastName = 'Kumar';
+console.log(found);
+console.log("the grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 88.66;
+let firstName: string = "Anup";
+let lastName: string = 'Kumar'
+
+console.log(found);
+console.log("the grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
