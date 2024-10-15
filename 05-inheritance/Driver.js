444c146af99e54080c1b745f8205f1c8538311c5
haein_sung <blue6zombie6@gmail.com>
2024-10-15 15:52:27 +0900

    inheritance 2/2

--- 05-inheritance/Driver.js
+++ 05-inheritance/Driver.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Shape_1 = require("./Shape");
-var Circle_1 = require("./Circle");
-var myShape = new Shape_1.Shape(10, 15);
+const Shape_1 = require("./Shape");
+const Circle_1 = require("./Circle");
+const Rectangle_1 = require("./Rectangle");
+let myShape = new Shape_1.Shape(10, 15);
 console.log(myShape.getInfo());
-var myCircle = new Circle_1.Circle(5, 10, 20);
+let myCircle = new Circle_1.Circle(5, 10, 20);
 console.log(myCircle.getInfo());
+let myRectangle = new Rectangle_1.Rectangle(0, 0, 3, 7);
+console.log(myRectangle.getInfo());
