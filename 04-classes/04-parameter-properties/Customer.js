25ec58b1b17f8ede7d7bbf22842e4632368f0ef9
haein_sung <blue6zombie6@gmail.com>
2024-10-15 15:09:29 +0900

    ~ parameter properties (축약형)

---
+++ 04-classes/04-parameter-properties/Customer.js
@@ -0,0 +1,32 @@
+var Customer = /** @class */ (function () {
+    function Customer(_firstName, _lastName) {
+        this._firstName = _firstName;
+        this._lastName = _lastName;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
+var myCustomer = new Customer("Martin", "Dixon");
+myCustomer.firstName = "Susan";
+myCustomer.lastName = "Bones";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
--- 04-classes/04-parameter-properties/Customer.ts
+++ 04-classes/04-parameter-properties/Customer.ts
@@ -1,7 +1,7 @@
 class Customer {
 
-    private _firstName: string;
-    private _lastName: string;
+    constructor(private _firstName: string, private _lastName: string) {
+    }
 
     get firstName(): string {
         return this._firstName;
@@ -17,11 +17,6 @@ class Customer {
 
     set lastName(value: string) {
         this._lastName = value;
-    }
-
-    constructor(theFirst: string, theLast: string) {
-        this._firstName = theFirst;
-        this._lastName = theLast;
     }
 }
 
